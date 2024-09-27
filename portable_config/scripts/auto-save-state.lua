df8894171f817ae6c351d24fc9853fb8cd181278
itsmeipg <isaiahpgiron@gmail.com>
2024-09-27 15:46:49 -0500

    Update auto-save-state.lua

--- portable_config/scripts/auto-save-state.lua
+++ portable_config/scripts/auto-save-state.lua
@@ -9,34 +9,14 @@ end)
 
 mp.set_property("save-position-on-quit", "yes")
 
-local function save()
-    if mp.get_property_bool("resume-playback") then
-        mp.command("write-watch-later-config")
-    end
-end
-
-local function save_on_file_loaded()
-    if mp.get_property_number("playlist-pos") == 0 then
-        return
-    end
-    save()
-end
-
 local function save_if_pause(_, pause)
     if pause then
-        save()
-    end
-end
-
-local function pause_timer_while_paused(_, pause)
-    if pause then
         timer:stop()
+        mp.command("write-watch-later-config")
     else
         timer:resume()
     end
 end
 
 timer = mp.add_periodic_timer(options.save_interval, save)
-
-mp.observe_property("pause", "bool", pause_timer_while_paused)
 mp.observe_property("pause", "bool", save_if_pause)
