1a0998cde2f49eb9634dfa9106fdc1b68e946845
itsmeipg <isaiahpgiron@gmail.com>
2024-09-27 05:49:31 -0500

    Update uosc-screenshot.lua

--- portable_config/scripts/uosc-screenshot.lua
+++ portable_config/scripts/uosc-screenshot.lua
@@ -1,8 +1,13 @@
+local options = {
+    include_subs = false
+}
+
 local script_name = mp.get_script_name()
 
 mp.utils = require "mp.utils"
-
-subs = false
+mp.options = require "mp.options"
+mp.options.read_options(options, "uosc-video-settings", function()
+end)
 
 function command(str)
     return string.format("script-message-to %s %s", script_name, str)
@@ -11,7 +16,7 @@ end
 function create_menu_data()
     local items = {{
         title = "Include subtitles",
-        icon = subs and "check_box" or "check_box_outline_blank",
+        icon = options.include_subs and "check_box" or "check_box_outline_blank",
         value = command("toggle-subs"),
         keep_open = true
     }, {
@@ -33,20 +38,21 @@ function update_menu()
 end
 
 mp.register_script_message("toggle-subs", function()
-    subs = not subs
+    options.include_subs = not options.include_subs
     update_menu()
 end)
 
 mp.register_script_message("screenshot", function()
-    if subs then
+    if options.include_subs then
         mp.command("no-osd screenshot")
     else
         mp.command("no-osd screenshot video")
     end
 end)
 
+-- Main execution/binding
 mp.add_forced_key_binding(nil, "open-menu", function()
-    subs = false
+    options.include_subs = false
 
     local json = mp.utils.format_json(create_menu_data())
     mp.commandv("script-message-to", "uosc", "open-menu", json)
