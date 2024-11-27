7e1837288569a91caeb20fbe92eb12cb9fdfa465
itsmeipg <isaiahpgiron@gmail.com>
2024-11-27 09:59:19 -0600

    Update auto-save-state.lua

--- portable_config/scripts/auto-save-state.lua
+++ portable_config/scripts/auto-save-state.lua
@@ -1,5 +1,4 @@
 -- Save state in multiple scenarios and control deletion
-
 local options = {
     timer_enabled = true,
     auto_save_interval = 1,
@@ -11,20 +10,20 @@ mp.options.read_options(options, "auto-save-state")
 
 mp.set_property("save-position-on-quit", "yes")
 
-local function save()
-    mp.command("write-watch-later-config")
-end
-
 local loaded_file_path
 local eof_reached = false
-local timer
-if options.timer_enabled then
-    timer = mp.add_periodic_timer(options.auto_save_interval, save)
+
+local function save()
+    if not eof_reached or eof_reached and not options.delete_finished then
+        mp.command("write-watch-later-config")
+    end
 end
+
+local timer = mp.add_periodic_timer(options.auto_save_interval, save)
 
 local function timer_state(active)
     if timer then
-        if active then
+        if active and options.timer_enabled and not eof_reached then
             timer:resume()
         else
             timer:stop()
@@ -34,17 +33,16 @@ end
 
 mp.register_event("file-loaded", function()
     loaded_file_path = mp.get_property("path")
-    if timer then
-        timer.timeout = options.auto_save_interval
-    end
+    timer.timeout = options.auto_save_interval
+    timer_state(true)
     save()
 end)
 
 mp.observe_property("pause", "bool", function(name, pause)
     if pause then
-        save()
         timer_state(false)
-    elseif not eof_reached then
+        save()
+    else
         timer_state(true)
     end
 end)
@@ -70,7 +68,7 @@ mp.observe_property("eof-reached", "bool", function(name, eof)
 end)
 
 mp.add_hook("on_unload", 50, function()
-    if not eof_reached and not options.delete_unloaded then
+    if not options.delete_unloaded then
         save()
     end
 end)
@@ -81,5 +79,13 @@ mp.register_event("end-file", function(event)
             print("Deleting state (end-file " .. event["reason"] .. ").")
             mp.commandv("delete-watch-later-config", loaded_file_path)
         end
+    end
+end)
+
+mp.observe_property("idle-active", "bool", function(name, idle)
+    if idle then
+        timer_state(true)
+    else
+        timer_state(false)
     end
 end)
